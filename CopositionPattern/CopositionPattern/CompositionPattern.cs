6de776f5f423045e47ebbf9d0aa89a449e73a918
o_sargazi <o_sargazi@DESKTOP-ELI44GB>
2025-07-19 11:04:11 +0330

    Composition Pattern for Menu Of a Resturant

--- CopositionPattern/CopositionPattern/CompositionPattern.cs
+++ CopositionPattern/CopositionPattern/CompositionPattern.cs
@@ -6,7 +6,5 @@ using System.Threading.Tasks;
 
 namespace CopositionPattern
 {
-    public interface CompositionPattern
-    {
-    }
+   
 }
---
+++ CopositionPattern/CopositionPattern/CompositionPattern/IMenuComponent.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopositionPattern.CompositionPattern
+{
+    public interface IMenuComponent
+    {
+        void Display(int indent =0);
+    }
+
+    public class MenuItem : IMenuComponent
+    {
+        private readonly string _title;
+        public MenuItem(string title)
+        {
+            _title = title;
+        }
+        public void Display(int indent = 0)
+        {
+           Console.WriteLine(new string(' ',indent)+"_"+_title);
+        }
+    }
+
+    public class Menu : IMenuComponent
+    {
+        private readonly string _title;
+        private readonly List<IMenuComponent> _items = new();
+        public Menu(string title)
+        {
+            _title=title;
+        }
+
+        public void Add(IMenuComponent item)
+        {
+            _items.Add(item);
+        }
+        public void Display(int indent = 0)
+        {
+            Console.WriteLine(new string(' ',indent) +_title);
+            foreach(var item in _items)
+            {
+                item.Display(indent+2);
+            }
+        }
+    }
+
+
+    public class CompositeClient
+    {
+        public static void Display()
+        {
+            var burger = new MenuItem("burger");
+            var fries = new MenuItem("Fries");
+            var juice = new MenuItem("Huice");
+
+
+            var kidsmenu = new Menu("Kids Menu");
+            kidsmenu.Add(juice);
+
+            var mainMenu = new Menu("Main Menu");
+            mainMenu.Add(burger);
+            mainMenu.Add(fries);
+            mainMenu.Add(kidsmenu);
+
+            mainMenu.Display();
+            
+        }
+    }
+}
