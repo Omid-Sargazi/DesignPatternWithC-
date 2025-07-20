1438cdd942d1e96fe537bf1a6c7de0e93c6d45bd
o_sargazi <o_sargazi@DESKTOP-ELI44GB>
2025-07-20 06:22:11 +0330

    Chain Of Responsibility By Delegate

---
+++ PatternsWithCSharp/ChainOfResponsibility/Resume.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternsWithCSharp.ChainOfResponsibility
+{
+    public class Resume
+    {
+        public string CandidateName { get; set; }
+        public int YearsOfExperience { get; set; }
+        public string EnglishLevel { get; set; }
+        public int ExpectedSalary { get; set; }
+    }
+
+    public enum ResumeStatus
+    {
+        Accepted,
+        RejectedByExperience,
+        RejectedByEnglishLevel,
+        RequiresManagerApproval
+    }
+
+    public delegate ResumeStatus? ResumeCheck(Resume resume);
+    
+    public class ResumeProcessor
+    {
+        private List<ResumeCheck> _checks = new();
+        public ResumeProcessor AddCheck(ResumeCheck check) 
+        {
+            _checks.Add(check);
+            return this;
+        }
+
+        public ResumeStatus Process(Resume resume)
+        {
+            foreach(var check in _checks)
+            {
+                var result = check(resume);
+                if(result.HasValue && result.Value !=ResumeStatus.Accepted)
+                {
+                    return result.Value;
+                }
+            }
+            return ResumeStatus.Accepted;
+        }
+    }
+}
