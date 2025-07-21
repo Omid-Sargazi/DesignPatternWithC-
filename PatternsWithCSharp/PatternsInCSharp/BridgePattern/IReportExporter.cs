3694b83311bbae0e741c43a57efcbfacd14aacc5
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-21 07:25:15 +0330

    Create implementation of bridge pattern

---
+++ PatternsWithCSharp/PatternsInCSharp/BridgePattern/IReportExporter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternsInCSharp.BridgePattern
+{
+    public interface IReportExporter
+    {
+        void Export(string reportType, string content);
+    }
+
+    public class PDFExporter : IReportExporter
+    {
+        public void Export(string reportType, string content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ExcelExporter : IReportExporter
+    {
+        public void Export(string reportType, string content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public abstract class Report
+    {
+        protected IReportExporter _exporter;
+        protected Report(IReportExporter exporter)
+        { 
+            _exporter = exporter; 
+        }
+
+        public abstract void Generate(string content);
+    }
+}
