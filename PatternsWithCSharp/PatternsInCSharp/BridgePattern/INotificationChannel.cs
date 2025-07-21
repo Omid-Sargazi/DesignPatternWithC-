a93e67cb1b1522b781d5fcd05c5df7ab73bbb13a
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-21 06:33:49 +0330

    Create Implementation of Bridge Pattern

---
+++ PatternsWithCSharp/PatternsInCSharp/BridgePattern/INotificationChannel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternsInCSharp.BridgePattern
+{
+    public interface INotificationChannel
+    {
+        void Send(string message);
+    }
+
+    public class EmailChannel : INotificationChannel
+    {
+        public void Send(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class SMSChannel : INotificationChannel
+    {
+        public void Send(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
