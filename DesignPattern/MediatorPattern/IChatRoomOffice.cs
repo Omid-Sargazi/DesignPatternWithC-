c0b407039fa16cc731db975c75f3bb1520a2e892
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-26 06:39:57 +0330

    Complete mediator office chat

--- DesignPattern/MediatorPattern/IChatRoomOffice.cs
+++ DesignPattern/MediatorPattern/IChatRoomOffice.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,7 +33,17 @@ namespace DesignPattern.MediatorPattern
 
         public void SendGroupMessage(string message, IEmployee sender, List<IEmployee> recipients)
         {
-            throw new NotImplementedException();
+           foreach(var recipient in recipients)
+            {
+                if (_employees.Contains(recipient))
+                {
+                    recipient.ReceiveMessage(message, sender);
+                }
+                else
+                {
+                    Console.WriteLine($"[خطا] کارمند {recipient.Name} در سیستم وجود ندارد!");
+                }
+            }
         }
 
         public void SendPrivateMessage(string message, IEmployee sender, IEmployee recipient)
@@ -43,6 +54,36 @@ namespace DesignPattern.MediatorPattern
             }
            else
                 Console.WriteLine($"[خطا] کارمند {recipient.Name} در سیستم وجود ندارد!");
+        }
+    }
+
+    public class Employee : IEmployee
+    {
+        public string Name { get; }
+
+        public string Department {  get; }
+        private IChatRoomOfficee _chatroom;
+
+        public Employee(IChatRoomOfficee chatRoom, string name, string department)
+        {
+            _chatroom = chatRoom;
+            Name = name;
+            Department = department;
+        }
+
+        public void ReceiveMessage(string message, IEmployee sender)
+        {
+            Console.WriteLine($"{Name} (از {Department}): پیامی از {sender.Name} (در {sender.Department}) دریافت کرد:\n{message}\n");
+        }
+
+        public void SendGroupMessage(string message, List<IEmployee> recipients)
+        {
+            _chatroom.SendGroupMessage(message, this, recipients);
+        }
+
+        public void SendProvateMessage(string message, IEmployee recipient)
+        {
+            _chatroom.SendPrivateMessage(message, this, recipient);
         }
     }
 }
