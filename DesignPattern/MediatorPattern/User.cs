e6869a9f55bcd0843664700136c06c455248f87b
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-26 06:04:28 +0330

    Create Interfaces for User and Chatroom for mediator Pattern

---
+++ DesignPattern/MediatorPattern/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.MediatorPattern
+{
+    public interface IUser
+    {
+        string Name { get; set; }
+        void SendGroupMessage(string message, IUser user);
+        void ReceiveMessage(string message, IUser user);
+    }
+
+    public interface IChatRoom
+    {
+        void RegisterUser(IUser user);
+        void SendMessage(string message, IUser sender, IUser recipient);
+        void SendGroupMessage(string message, IUser sender);
+    }
+
+    public class ChatRoom : IChatRoom
+    {
+        
+        public void RegisterUser(IUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendGroupMessage(string message, IUser sender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMessage(string message, IUser sender, IUser recipient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
