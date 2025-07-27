f7cb749a304a7e03373b85b7b1ea5566fb688358
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-27 07:58:44 +0330

    Top K Largest Min Heap

--- DesignPattern/LeetCodeProblems/ArrayTraversal.cs
+++ DesignPattern/LeetCodeProblems/ArrayTraversal.cs
@@ -10,18 +10,40 @@ namespace DesignPattern.LeetCodeProblems
     {
         public int FindMaximum(int[] nums)
         {
-            if(nums == null || nums.Length==0)
+            if (nums == null || nums.Length == 0)
             {
                 throw new ArgumentNullException("Array must not be empty.");
             }
 
             int maxVal = nums[0];
-            foreach (int num in nums) 
-            { 
-                if(num >maxVal)
+            foreach (int num in nums)
+            {
+                if (num > maxVal)
                     maxVal = num;
             }
 
             return maxVal;
+        }
+    
+
+    public int[] TopKLargest_Sort(int[] nums, int k)
+        {
+            return nums.OrderByDescending(x => x).Take(k).ToArray();
+        }
     }
+
+    public int[] TopKLargest_MinHeap(int[] nums, int k)
+        {
+            var minHeap = new PriorityQueue<int,int>();
+            foreach(int num in nums)
+            {
+                minHeap.Enqueue(num, num);
+                if(minHeap.Count>k)
+                {
+                    minHeap.Dequeue();
+                }
+            }
+
+            return minHeap.UnorderedItems.Select(x => x.Element).ToArray();
+        }
 }
