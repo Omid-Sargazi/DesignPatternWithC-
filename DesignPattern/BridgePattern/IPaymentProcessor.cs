87ac6e39c091c5138dfe4887e5456d88825992f9
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-27 06:11:07 +0330

    Implement all paymant ways

---
+++ DesignPattern/BridgePattern/IPaymentProcessor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.BridgePattern
+{
+    public interface IPaymentProcessor
+    {
+        void ProcessPayment(decimal amount);
+    }
+
+    public class CreditCardPayment : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"پرداخت با کارت اعتباری به مبلغ {amount} تومان انجام شد.");
+        }
+    }
+
+    public class PayPalPayment : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"پرداخت با PayPal به مبلغ {amount} تومان انجام شد.");
+        }
+    }
+
+    public class BankTransferPayment : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"پرداخت بانکی به مبلغ {amount} تومان انجام شد.");
+        }
+    }
+}
