fc6dc3dc6eb20841bc357ecc3b099e3516b9007b
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-27 06:15:28 +0330

    Complete the part of Abstarction of the bridge pattern

--- DesignPattern/BridgePattern/IPaymentProcessor.cs
+++ DesignPattern/BridgePattern/IPaymentProcessor.cs
@@ -34,4 +34,46 @@ namespace DesignPattern.BridgePattern
             Console.WriteLine($"پرداخت بانکی به مبلغ {amount} تومان انجام شد.");
         }
     }
+
+    public abstract class PaymentManager
+    {
+        protected IPaymentProcessor _processor;
+        protected PaymentManager(IPaymentProcessor processor)
+        {
+            _processor = processor;
+        }
+
+        public abstract void ProcessPayment(decimal amount);
+    }
+
+    public class WebPaymentManager : PaymentManager
+    {
+        private readonly decimal _amount;
+       
+        public WebPaymentManager(decimal amount,IPaymentProcessor processor) : base(processor)
+        {
+            _amount = amount;
+        }
+
+        public override void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine("پرداخت در پلتفرم وب:");
+            _processor.ProcessPayment(amount);
+        }
+    }
+
+    public class MobilePaymentManager : PaymentManager
+    {
+        private readonly decimal _amount;
+        public MobilePaymentManager(decimal amount,IPaymentProcessor processor) : base(processor)
+        {
+            _amount = amount;
+        }
+
+        public override void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine("پرداخت در اپلیکیشن موبایل:");
+            _processor.ProcessPayment(amount);
+        }
+    }
 }
