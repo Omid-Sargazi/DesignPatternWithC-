3c5e2248096fff40401d3305875fe8767d9789b5
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-27 07:47:10 +0330

    Find Maximum in a list on O(1) complixity

---
+++ DesignPattern/LeetCodeProblems/ArrayTraversal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.LeetCodeProblems
+{
+    public class ArrayTraversal
+    {
+        public int FindMaximum(int[] nums)
+        {
+            if(nums == null || nums.Length==0)
+            {
+                throw new ArgumentNullException("Array must not be empty.");
+            }
+
+            int maxVal = nums[0];
+            foreach (int num in nums) 
+            { 
+                if(num >maxVal)
+                    maxVal = num;
+            }
+
+            return maxVal;
+    }
+}
