d4b3e1a5a5737e50dd7d455cd076d7e444e45b36
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-27 06:27:42 +0330

    Created all Implementation of the pattern

---
+++ DesignPattern/BridgePattern/IRenderEngine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.BridgePattern
+{
+    public interface IRenderEngine
+    {
+        void RenderShape(string shapeName);
+    }
+
+    public class OpenGLRender : IRenderEngine
+    {
+        public void RenderShape(string shapeName)
+        {
+            Console.WriteLine($"رندر {shapeName} با موتور OpenGL");
+        }
+    }
+    public class DirectXRenderer : IRenderEngine
+    {
+        public void RenderShape(string shapeName)
+        {
+            Console.WriteLine($"رندر {shapeName} با موتور DirectX");
+        }
+    }
+
+    public class VulkanRenderer : IRenderEngine
+    {
+        public void RenderShape(string shapeName)
+        {
+            Console.WriteLine($"رندر {shapeName} با موتور Vulkan");
+        }
+    }
+
+    
+}
