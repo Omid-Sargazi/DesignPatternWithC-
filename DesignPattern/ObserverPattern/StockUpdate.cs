e21b3553fd5b1a611585a8d2907bf27129d9775b
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-03 06:21:13 +0330

    Kind Of Notifications and information related to notify

---
+++ DesignPattern/ObserverPattern/StockUpdate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.ObserverPattern
+{
+    public enum NotificationType
+    {
+        PriceChange,
+        VolumeChange,
+        SignificantMovement,
+        NewsAlert,
+    }
+    public class StockUpdate
+    {
+        public string Symbol { get; }
+        public decimal Price { get; }
+        public decimal ChangePercent { get; }
+        public long Volume {  get; }
+        public DateTime UpdateTime { get; }
+
+        public StockUpdate(string symbol, decimal price, decimal changePercent, long volume)
+        {
+            Symbol = symbol;
+            Price = price;
+            ChangePercent = changePercent;
+            Volume = volume;
+            UpdateTime = DateTime.Now;
+        }
+    }
+}
