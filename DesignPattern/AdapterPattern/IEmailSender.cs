94d8b4265bf7a0e9a3381967d3df011b622298f8
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-04 06:15:10 +0330

    Adapter Pattern for Email Servicesd and External Email Services

---
+++ DesignPattern/AdapterPattern/IEmailSender.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.AdapterPattern
+{
+    public interface IEmailSender
+    {
+        void SendEmail(string to, string subject,string body);
+    }
+
+    public class ExternalMailService
+    {
+        public void Send(string recipient, string title,string message)
+        {
+            Console.WriteLine("✅ Email sent via ExternalMailService.");
+        }
+    }
+
+    public class EmailAdapter : IEmailSender
+    {
+        private readonly ExternalMailService _service;
+        public EmailAdapter(ExternalMailService service)
+        {
+            _service = service;
+        }
+        public void SendEmail(string to, string subject, string body)
+        {
+           _service.Send(to, subject, body);
+        }
+    }
+}
