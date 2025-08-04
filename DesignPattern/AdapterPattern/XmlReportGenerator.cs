51af5cbb181109fc24bc6596b6d6112ed13eea56
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-04 06:24:49 +0330

    Convert Xml Data into Json By Adapter pattern

---
+++ DesignPattern/AdapterPattern/XmlReportGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DesignPattern.AdapterPattern
+{
+    public class XmlReportGenerator
+    {
+        public void GenerateReport(string xmlData)
+        {
+            Console.WriteLine("📄 Report generated with XML:\n" + xmlData);
+        }
+    }
+
+    public class JsonDataProvider
+    {
+        public string GetJson()
+        {
+            return "{ \"name\": \"Omid\", \"score\": 95 }";
+        }
+    }
+
+    public class JsonToXmlAdapter
+    {
+        private readonly JsonDataProvider _provider;
+        public JsonToXmlAdapter(JsonDataProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public string GetXmlData()
+        {
+            var doc = _provider.GetJson();
+            return doc?.ToString();
+        }
+    }
+}
