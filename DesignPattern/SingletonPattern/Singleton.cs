fbf1749713905a7a7fcb8a8a0dc06c3a92962a94
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-04 06:05:45 +0330

    Singleton Patttern

---
+++ DesignPattern/SingletonPattern/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.SingletonPattern
+{
+    public class Singleton
+    {
+        private static volatile Singleton instance;
+        private static object lockObject = new object();
+        private Singleton() { }
+
+        public static Singleton Instance { get
+            {
+                if(instance==null)
+                {
+                    lock(lockObject)
+                    {
+                        if(instance == null)
+                        {
+                            instance = new Singleton();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+    }
+}
