ba3c9233bcc6695b5dd5bf934ef55a05a750a9c1
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-09 06:37:38 +0330

    Paymnet Way without Factory Method and use If and else

---
+++ DesignPattern/FactyoryMethod/IPayment.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.FactyoryMethod
+{
+    public interface IPayment
+    {
+        void Pay(decimal amount);
+    }
+
+    public class PayPalPayment : IPayment
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Paid {amount} via PayPal");
+        }
+    }
+
+    public class StripePayment : IPayment
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Paid {amount} via Stripe");
+        }
+    }
+
+    public class FactoryMethod
+    {
+        public static void Run()
+        {
+            string method = "paypal";
+            IPayment payment;
+            if(method == "paypal")
+            {
+                payment = new PayPalPayment();
+            }
+           else
+            {
+                payment = new StripePayment();
+            }
+
+            payment.Pay(100);
+        }
+    }
+}
