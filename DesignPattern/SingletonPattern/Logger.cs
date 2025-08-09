975c7220a9a4a05f6484b35798b5b2a808322db5
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-09 06:04:37 +0330

    Generate Report By Bridge Pattern In C#

---
+++ DesignPattern/SingletonPattern/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.SingletonPattern
+{
+    public sealed class Logger
+    {
+        private readonly Lazy<Logger> _logger = new(()=>new Logger());
+        private Logger()
+        {
+            Console.WriteLine("Initi Logger");
+        }
+    }
+}
