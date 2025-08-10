b5542a66b340802c06d1e17b9ba316c399eb1d4f
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-10 06:08:58 +0330

    Interfaces in Abstract Factory

---
+++ DesignPattern/AbstarctFactory/IPaymentProcessor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.AbstarctFactory
+{
+    public interface IPaymentProcessor
+    {
+        Task ProcessAsync(decimal amount, string currency);
+    }
+
+    public interface IReceiptGenerator
+    {
+        string Generate(decimal amount, string currency);
+    }
+
+    public class PayPalProcessor : IPaymentProcessor
+    {
+        public Task ProcessAsync(decimal amount, string currency)
+        {
+            Console.WriteLine($"[PayPal] Paid {amount} {currency}");
+            return Task.CompletedTask;
+        }
+    }
+
+    public class PayPalReceipt : IReceiptGenerator
+    {
+        public string Generate(decimal amount, string currency)
+        {
+            return $"[PayPal] Receipt for {amount} {currency}";
+        }
+    }
+
+    public class StripeProcessor : IPaymentProcessor
+    {
+        public Task ProcessAsync(decimal amount, string currency)
+        {
+            Console.WriteLine($"[Stripe] Paid {amount} {currency}");
+            return Task.CompletedTask;
+        }
+    }
+
+    public class StripeReceipt : IReceiptGenerator
+    {
+        public string Generate(decimal amount, string currency)
+        {
+            return $"[Stripe] Receipt for {amount} {currency}";
+        }
+    }
+}
