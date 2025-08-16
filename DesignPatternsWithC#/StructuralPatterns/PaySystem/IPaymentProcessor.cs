06dde53bc44593946df94d55f7dc8cf9c0ba9323
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-16 06:47:26 +0330

    Adapter Pattern In C# For Paying System

---
+++ DesignPatternsWithC#/StructuralPatterns/PaySystem/IPaymentProcessor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsWithC_.StructuralPatterns.PaySystem
+{
+    public interface IPaymentProcessor
+    {
+        bool ProcessPayment(double amount);
+        bool Refund(double amount);
+    }
+
+    public class LegacyPaymentAdapter : IPaymentProcessor
+    {
+        private readonly LegacyPaymentSystem _legacyPaymentSystem;
+        public LegacyPaymentAdapter(LegacyPaymentSystem legacyPaymentSystem)
+        {
+            _legacyPaymentSystem = legacyPaymentSystem;
+        }
+        public bool ProcessPayment(double amount)
+        {
+            _legacyPaymentSystem.MakeTransaction(amount);
+            return true;
+        }
+
+        public bool Refund(double amount)
+        {
+            _legacyPaymentSystem.CancelTransaction(amount);
+            return true;
+        }
+    }
+}
---
+++ DesignPatternsWithC#/StructuralPatterns/PaySystem/LegacyPaymentSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsWithC_.StructuralPatterns.PaySystem
+{
+    public class LegacyPaymentSystem
+    {
+        public bool MakeTransaction(double amount)
+        {
+            Console.WriteLine($"Legacy system processing transaction of ${amount}");
+            return true;
+        }
+        public bool CancelTransaction(double amount) 
+        {
+            Console.WriteLine($"Legacy system canceling transaction of ${amount}");
+            return true;
+        }
+    }
+}
