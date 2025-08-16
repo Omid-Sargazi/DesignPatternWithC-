57d003dabc50e6d4254586087ad1f12b260b91fd
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-16 06:14:16 +0330

    Bubble Sort In C#

--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using LeetCodeProblems.Problem1;
+
 Console.WriteLine("Hello, World!");
+
+int[] arr = { 10,-10,9,7,100,14};
+BubbleSort.Run(arr);
