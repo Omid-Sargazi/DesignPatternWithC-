e474bb2d89060b9c522f52d977b0001ca6f6c971
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-17 06:05:49 +0330

    Insertion Sort With C Sharp

---
+++ LeetCodeProblems/SortingInCSharp/InsertionSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.SortingInCSharp
+{
+    public class InsertionSort
+    {
+        public static void Run(int[] arr)
+        {
+            for(int i=1;i<=arr.Length;i++)
+            {
+                int j = i - 1;
+                int key = arr[i];
+                while(j>=0 && arr[j] > key)
+                {
+                    j--;
+                    arr[j+1]=arr[j];
+                }
+                arr[j+1] = key;
+            }
+
+            foreach(var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
