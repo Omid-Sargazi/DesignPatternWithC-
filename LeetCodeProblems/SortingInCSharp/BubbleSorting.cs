a5163bc3c225dbc6caa7037f062feb4fc55d1475
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-17 06:11:22 +0330

    Insertion Sort Another Problem

--- LeetCodeProblems/SortingInCSharp/BubbleSorting.cs
+++ LeetCodeProblems/SortingInCSharp/BubbleSorting.cs
@@ -31,4 +31,25 @@ namespace LeetCodeProblems.SortingInCSharp
             }
         }
     }
+
+    public class SortingByCSharpInsertion
+    {
+        public static void Run(int[] arr)
+        {
+            for(int i=1;i<=arr.Length;i++)
+            {
+                int j = i - 1;
+                int key = arr[i];
+                while (j >= 0 && arr[j]>key)
+                    {
+                    arr[j+1] = arr[j];
+                    j--;
+                    }
+                arr[j + 1] = key;
+
+            }
+
+            
+        }
+    }
 }
