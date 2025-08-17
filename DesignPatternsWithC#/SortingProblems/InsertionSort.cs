16f6153787630e31890c598ca79abc00e53a3fe2
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-17 06:37:51 +0330

    Insertion Sort With C#

---
+++ DesignPatternsWithC#/SortingProblems/InsertionSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsWithC_.SortingProblems
+{
+    public class InsertionSort
+    {
+        public static void Run(int[] arr)
+        {
+            for(int i=1;i<=arr.Length;i++)
+            {
+                int j = i - 1;
+                int key = arr[i];
+                while(j>=0 && arr[j]>key)
+                {
+                    arr[j+1]= arr[j];
+                    j--;
+                }
+                arr[j+1]=key;
+            }
+        }
+    }
+}
