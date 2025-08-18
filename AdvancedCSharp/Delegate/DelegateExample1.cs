53c00a630d7bad2ff95921f5e04a20bda898aedd
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-18 06:18:00 +0330

    Simple Delegate Example

---
+++ AdvancedCSharp/Delegate/DelegateExample1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedCSharp.Delegate
+{
+    public class DelegateExample1
+    {
+        public delegate void ShowMessage(string message);
+
+            ShowMessage messageDelegate = DisplayMessage;
+        public static void Run()
+        {
+            
+        }
+
+        public static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
--- AdvancedCSharp/Program.cs
+++ AdvancedCSharp/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AdvancedCSharp.Delegate;
+
 Console.WriteLine("Hello, World!");
+//DelegateExample1.Run();
+DelegateExample1.DisplayMessage("hIII");
