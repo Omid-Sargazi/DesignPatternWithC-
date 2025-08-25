dcee720b4f460675c604419cd35c73d9145b4d48
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-25 06:08:27 +0330

    Selection Sort In C#

---
+++ AdvancedCSharp/DataStructure/InsertionSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedCSharp.DataStructure
+{
+    public class SelectionSort
+    {
+    public static void Run(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[j] < arr[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            (arr[i], arr[minIndex]) = (arr[minIndex], arr[i]);
+        }
+
+
+        Console.WriteLine(string.Join(",",arr));
+    }
+    }
+}
