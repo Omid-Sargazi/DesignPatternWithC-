cb84ea5c128f7548a169cfa7f77b0e676a5f70a1
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-26 06:05:09 +0330

    Bubble Sort in C#

---
+++ DataStructure/Sorting/BubbleSorting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Sorting
+{
+    public class BubbleSorting
+    {
+        public static void RunBubble(int[] arr)
+        {
+            for (int start = arr.Length - 1; start >= 0; start--)
+            {
+                bool swapped = false;
+                for (int j = 0; j < start; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        (arr[j], arr[j+1]) = (arr[j+1],arr[j]);
+                        swapped = true;
+                    }
+                }
+
+                if(!swapped) break;
+            }
+
+            Console.WriteLine(string.Join(",",arr));
+        }
+    }
+}
