71a74a927b1606179be5a2d7ab7a7af352ef0fd2
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-26 06:13:16 +0330

    Adapter Pattern in C Sharp

---
+++ DataStructure/Patterns/Adapter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Patterns
+{
+    public class Adaptee
+    {
+        public string getSpecificRequest()
+        {
+            return "Specific Request";
+        }
+    }
+
+    public interface ITarget
+    {
+        public string GetRequest();
+    }
+    public class Adapter : ITarget
+    {
+        private readonly Adaptee _adaptee;
+
+        public Adapter(Adaptee adaptee)
+        {
+            _adaptee = adaptee;
+        }
+        public string GetRequest()
+        {
+            return $"This is {_adaptee.getSpecificRequest()}";
+        }
+    }
+
+    public class AdapterClient
+    {
+        public static void Run()
+        {
+            Adaptee adaptee = new Adaptee();
+            Adapter adapter = new Adapter(adaptee);
+            Console.WriteLine(adapter.GetRequest());
+        }
+    }
+}
