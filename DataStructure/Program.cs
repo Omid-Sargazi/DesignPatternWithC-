80958f8628a2493dc02524bf7e4896baf7db2be1
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-26 06:05:40 +0330

    Divide and Conqure In C# for finding Max

---
+++ DataStructure/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DataStructure.Sorting;
+
+Console.WriteLine("Hello, World!");
+int[] arr = new[] { 70, -70, 4, 1, 2, 3, 41, -70, -80, -99, -1000};
+BubbleSorting.RunBubble(arr);
+Console.WriteLine(DivideAndConqure.Run(arr,0,arr.Length-1));
---
+++ DataStructure/Sorting/DivideAndConqure.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Sorting
+{
+    public class DivideAndConqure
+    {
+        public static int Run(int[] arr,int start, int end)
+        {
+            if(start == end) return arr[start];
+
+            int mid = (end + start) / 2;
+
+            int maxleft = Run(arr, start, mid);
+            int maxRight = Run(arr, mid + 1, end);
+
+            return Math.Max(maxleft, maxRight);
+        }
+    }
+}
