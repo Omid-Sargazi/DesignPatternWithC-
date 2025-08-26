6a34b8c4a3fdf31041153d4f8aa11793e1e5631c
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-26 06:38:15 +0330

    BinarySearch for searching target

--- DataStructure/Sorting/DivideAndConqure.cs
+++ DataStructure/Sorting/DivideAndConqure.cs
@@ -31,5 +31,21 @@ namespace DataStructure.Sorting
 
             return leftSum + rightSum;
         }
+
+        public static int BinarySearch(int[] arr, int start, int end, int target)
+        {
+            if (start > end) return -1;
+
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) return mid;
+
+            else if (arr[mid] > target)
+                return BinarySearch(arr, start, mid - 1, target);
+            else
+            {
+                return BinarySearch(arr, mid + 1, end, target);
+            }
+        }
     }
 }
