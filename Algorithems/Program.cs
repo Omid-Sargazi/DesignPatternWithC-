fc758917bdf84480a094a0a807e71f3fb5886196
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-30 06:27:08 +0330

    Merge Sortin in C#

--- Algorithems/Program.cs
+++ Algorithems/Program.cs
@@ -6,4 +6,5 @@ Console.WriteLine("Hello, World!");
 int[] arr = new int[] {-7,7,-8,-9,8,9,4,1,10,12,41,10,45,-9,-8};
 //Sorting1.Sorting.Bubble(arr);
 //Sorting1.Sorting.Selection(arr);
-Sorting1.Sorting.Insertion(arr);
+//Sorting1.Sorting.Insertion(arr);
+Sorting1.Sorting.MergeSorting(arr);
