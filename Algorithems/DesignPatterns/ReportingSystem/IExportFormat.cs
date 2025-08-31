e914da069475930bb5feb7bf0a2005a1466f0dd5
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-31 06:18:57 +0330

    Implement the part of the bridge pattern is called Implementation

---
+++ Algorithems/DesignPatterns/ReportingSystem/IExportFormat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithems.DesignPatterns.ReportingSystem
+{
+    public interface IExportFormat
+    {
+        void GenerateFile(string data, string fileName);
+    }
+
+    public class PdfExport : IExportFormat
+    {
+        public void GenerateFile(string data, string fileName)
+        {
+            Console.WriteLine($"در حال تولید فایل PDF: {fileName}.pdf");
+            Console.WriteLine($"محتوای PDF: {data}");
+        }
+    }
+
+    public class ExcelExport : IExportFormat
+    {
+        public void GenerateFile(string data, string fileName)
+        {
+            Console.WriteLine($"در حال تولید فایل Excel: {fileName}.xlsx");
+            Console.WriteLine($"داده‌های Excel: {data}");
+        }
+    }
+
+    public class HtmlExport : IExportFormat
+    {
+        public void GenerateFile(string data, string fileName)
+        {
+            Console.WriteLine($"در حال تولید فایل HTML: {fileName}.html");
+            Console.WriteLine($"کد HTML: <html><body>{data}</body></html>");
+        }
+    }
+}
