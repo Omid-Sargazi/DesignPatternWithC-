e0e2b7e78cad48cbb0f978f21b57ad4ca3f7ca3f
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-31 06:26:44 +0330

    Refined Abstraction Classes

--- Algorithems/DesignPatterns/ReportingSystem/IExportFormat.cs
+++ Algorithems/DesignPatterns/ReportingSystem/IExportFormat.cs
@@ -50,5 +50,51 @@ namespace Algorithems.DesignPatterns.ReportingSystem
         public abstract void Generate();
     }
 
+    public class SalesReport : Report
+    {
+        private readonly string _salesDate;
+        public SalesReport(IExportFormat exportFormat, string salesDate) : base(exportFormat)
+        {
+            _salesDate = salesDate;
+        }
 
+        public override void Generate()
+        {
+            Console.WriteLine("جمع‌آوری داده‌های فروش...");
+            string reportContent = $"گزارش فروش: {_salesDate}";
+            _exportFormat.GenerateFile(reportContent,"SalesReport");
+        }
+    }
+
+    public class InventoryReport : Report
+    {
+        private readonly string _inventoryData;
+        public InventoryReport(IExportFormat exportFormat, string inventoryData) : base(exportFormat)
+        {
+            _inventoryData = inventoryData;
+        }
+
+        public override void Generate()
+        {
+            Console.WriteLine("جمع‌آوری داده‌های موجودی انبار...");
+            string reportContent = $"گزارش موجودی: {_inventoryData}";
+            _exportFormat.GenerateFile(reportContent, "InventoryReport");
+        }
+    }
+
+    public class FinancialReport : Report
+    {
+        private readonly string _financialData;
+        public FinancialReport(IExportFormat exportFormat, string financialData) : base(exportFormat)
+        {
+            _financialData = financialData;
+        }
+
+        public override void Generate()
+        {
+            Console.WriteLine("جمع‌آوری داده‌های مالی...");
+            string reportContent = $"گزارش مالی: {_financialData}";
+            _exportFormat.GenerateFile(reportContent, "FinancialReport");
+        }
+    }
 }
