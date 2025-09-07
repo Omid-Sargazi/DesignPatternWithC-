b4881594bc8fda37bacd3a1089c707cbf5d7ebfd
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-07 08:20:17 +0330

    Create File class

---
+++ DesignPattern/CompositePattern/File.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.CompositePattern
+{
+    public interface IFileSystemComponent
+    {
+        string Name { get; }
+        long GetSize();
+        void Display(string indent = "");
+    }
+    public class File : IFileSystemComponent
+    {
+        public string Name { get; }
+        private long  Size {get;}
+
+        public File(string name, long size)
+        {
+            Name = name;
+            Size = size;
+        }
+        public long GetSize()
+        {
+            return Size;
+        }
+
+        public void Display(string indent = "")
+        {
+            Console.WriteLine($"{indent}📄 {Name} ({Size} bytes)");
+        }
+    }
+}
