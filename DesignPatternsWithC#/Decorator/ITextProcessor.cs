8ae0fc599c2441f3d1c1a947fc9907889b5e57bb
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-13 06:24:48 +0330

    Decoration Pattern In C#

---
+++ DesignPatternsWithC#/Decorator/ITextProcessor.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsWithC_.Decorator
+{
+    public interface ITextProcessor
+    {
+        string Process(string txt);
+        string GetDescription();
+
+    }
+
+    public class BasicTextProcessor:ITextProcessor
+    {
+        public string Process(string txt)
+        {
+            return txt;
+        }
+
+        public string GetDescription()
+        {
+            return "Process base txt";
+        }
+    }
+
+    public abstract class TxtProcessDecorator : ITextProcessor
+    {
+        protected ITextProcessor _textProcessor;
+
+        public TxtProcessDecorator(ITextProcessor textProcessor)
+        {
+            _textProcessor = textProcessor;
+        }
+
+        public virtual string Process(string txt) => _textProcessor.Process(txt);
+
+        public virtual string GetDescription() => _textProcessor.GetDescription();
+    }
+
+    public class CompressionDecorator : TxtProcessDecorator
+    {
+        public CompressionDecorator(ITextProcessor textProcessor) : base(textProcessor)
+        {
+        }
+
+        public override string Process(string txt)
+        {
+            
+            return base.Process(txt) +"Compress Txt";
+        }
+
+        public override string GetDescription()
+        {
+            return base.GetDescription() + "Compress";
+        }
+
+        private string Compress(string txt)
+        {
+            return $"compressed {txt}";
+        }
+    }
+
+    public class EncryptionDecorator : TxtProcessDecorator
+    {
+        public EncryptionDecorator(ITextProcessor textProcessor) : base(textProcessor)
+        {
+        }
+
+        public override string GetDescription()
+        {
+            return base.GetDescription() +"Encription";
+        }
+
+        public override string Process(string txt)
+        {
+            return base.Process(txt) + "Encript";
+        }
+    }
+}
