0f132fc6a20f04f039dd688f3ab9befc67c7397a
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-15 06:13:42 +0330

    Adapter Pattern in C#

---
+++ DesignPatternsInCSharp/Adapter/ITarget.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.Adapter
+{
+    public interface ITarget
+    {
+        string GetRequest();
+    }
+
+    public class Adaptee
+    {
+        public string GetOldRequest()
+        {
+            return "Old Request from Old System";
+        }
+    }
+
+    public class Adapter : ITarget
+    {
+        private readonly Adaptee _adaptee;
+
+        public Adapter(Adaptee adaptee)
+        {
+            _adaptee = adaptee;
+        }
+        public string GetRequest()
+        {
+           return _adaptee.GetOldRequest();
+        }
+    }
+}
