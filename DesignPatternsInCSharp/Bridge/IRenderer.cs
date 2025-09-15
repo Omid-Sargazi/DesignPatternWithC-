523d3bb66526890e59487a04293eab10aab5b488
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-15 06:14:03 +0330

    Bridge Pattern In C#

---
+++ DesignPatternsInCSharp/Bridge/IRenderer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.Bridge
+{
+    public interface IRenderer
+    {
+        void RenderCircle(float radius);
+        void RenderSquare(float  side);
+    }
+
+    public class WindowsRender : IRenderer
+    {
+        
+        public void RenderCircle(float radius)
+        {
+            Console.WriteLine($"Render Circle by Window: radius{radius}");
+        }
+
+        public void RenderSquare(float side)
+        {
+            Console.WriteLine($"Render Square by Window: radius{side}");
+
+        }
+    }
+
+    public class MacRender : IRenderer
+    {
+        public void RenderCircle(float radius)
+        {
+            Console.WriteLine($"Render Circle by Mac: radius{radius}");
+        }
+
+        public void RenderSquare(float side)
+        {
+            Console.WriteLine($"Render Square by Mac: radius{side}");
+
+        }
+    }
+
+    public abstract class BaseRender
+    {
+        protected readonly IRenderer _renderer;
+
+        public BaseRender(IRenderer render)
+        {
+            _renderer = render;
+        }
+
+        public abstract void Draw();
+
+    }
+
+    public class Circle : BaseRender
+    {
+        private float _radius;
+
+      
+        public Circle(IRenderer render,float radius) : base(render)
+        {
+            _radius = radius;
+        }
+
+
+        public override void Draw()
+        {
+            _renderer.RenderCircle(_radius);
+        }
+    }
+
+    public class Rectangle : BaseRender
+    {
+        private float _side;
+
+       
+        public Rectangle(IRenderer render, float side) : base(render)
+        {
+            _side = side;
+        }
+
+        public override void Draw()
+        {
+            _renderer.RenderSquare(_side);
+        }
+    }
+}
