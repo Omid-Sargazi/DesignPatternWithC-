e2d00d305171c3e734aef2580cc033b17a606546
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-16 06:09:09 +0330

    Interfacer for Bridge Pattern

---
+++ AdvancedCSharp/DesignPattern/Structural/IPaymentGateway.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedCSharp.DesignPattern.Structural
+{
+    public interface IPaymentGateway
+    {
+        bool ProcessPayment(decimal amount, string currency);
+        bool RefundPayment(decimal amount, string transactionId);
+        string GetGatewayName();
+    }
+}
