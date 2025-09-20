8839dbb7d111ff220748e4b0d258602fd862c47d
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-20 06:04:45 +0330

    Create Node for BSTNode

---
+++ Algorithems/Trees/BSTNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithems.Trees
+{
+    public class BSTNode
+    {
+        public int Data;
+        public BSTNode Left;
+        public BSTNode Right;
+
+        public BSTNode(int data)
+        {
+            Data = data;
+            Left = Right = null;
+        }
+    }
+
+    public class BinarySearchTree
+    {
+        
+    }
+}
