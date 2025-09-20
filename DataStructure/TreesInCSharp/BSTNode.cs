9e216672b8f9c87c2abb644fa5da5f7b7b31437e
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-20 10:00:39 +0330

    Insert Data I BST

---
+++ DataStructure/TreesInCSharp/BSTNode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.TreesInCSharp
+{
+    public class BSTNode
+    {
+        public int Data;
+        public BSTNode Left;
+        public BSTNode Right;
+
+        public BSTNode(int data)
+        {
+            Data = data;
+            Left = Right = null;
+        }
+    }
+
+    public class BinarySearchTree
+    {
+        private BSTNode root;
+
+        public void Insert(int data)
+        {
+            root = InsertRec(root, data);
+        }
+
+        public BSTNode InsertRec(BSTNode node, int data)
+        {
+            if (node == null) return new BSTNode(data);
+
+            if (data < node.Data) node.Left = InsertRec(node.Left, data);
+            else if (data > node.Data) node.Right = InsertRec(node.Right, data);
+            return node;
+        }
+    }
+}
