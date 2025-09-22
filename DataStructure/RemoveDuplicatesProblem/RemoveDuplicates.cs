9c0af98f9959502cca9afe29a2cbcd7ddbe3a6a0
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-22 07:38:02 +0330

    Remove Duplicates in Soreted Array

---
+++ DataStructure/RemoveDuplicatesProblem/RemoveDuplicates.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.RemoveDuplicatesProblem
+{
+    public class RemoveDuplicates
+    {
+        public static int RemoveDuplicatesProblem(int[] nums)
+        {
+            if(nums.Length==0) return 0;
+
+            int uniquePointer = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[i - 1])
+                {
+                    nums[uniquePointer] = nums[i];
+                    uniquePointer++;
+                }
+            }
+
+            return uniquePointer;
+
+        }
+    }
+}
