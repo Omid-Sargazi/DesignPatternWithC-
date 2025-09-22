120f6b4c1067146068db547c97b3af93b694da60
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-22 05:59:30 +0330

    Add to end of LinkedList

---
+++ DataStructure/Linked List/Node.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Linked_List
+{
+    public class Node
+    {
+        public int Data;
+        public Node Next;
+
+        public Node(int data)
+        {
+            Data = data;
+        }
+    }
+
+    public class LinkedList
+    {
+        private Node head;
+
+        public LinkedList()
+        {
+            head = null;
+        }
+
+        public void AddLast(int data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next!=null)
+                {
+                    {
+                        current = current.Next;
+                    }
+                    current.Next = newNode;
+                }
+            }
+        }
+    }
+}
