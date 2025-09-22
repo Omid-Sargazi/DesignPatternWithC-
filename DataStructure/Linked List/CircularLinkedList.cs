52a3e374aa53db30be4fa11a9a80ddca29163bd2
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-22 06:27:53 +0330

    Circular Linked List

---
+++ DataStructure/Linked List/CircularLinkedList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Linked_List
+{
+    public class CircularLinkedList
+    {
+        private Node tail;
+
+        public CircularLinkedList()
+        {
+            tail = null;
+        }
+
+        public void AddLast(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (tail == null)
+            {
+                tail = newNode;
+                tail.Next = tail;
+            }
+            else
+            {
+                newNode.Next = tail.Next;
+                tail.Next = newNode;
+                tail = newNode;
+            }
+        }
+
+        public void AddFirst(int data)
+        {
+            Node newNode = new Node(data);
+            if (tail == null)
+            {
+                tail = newNode;
+                tail.Next = tail;
+            }
+            else
+            {
+                newNode.Next = tail.Next;
+                tail.Next = newNode;
+            }
+        }
+    }
+}
