ab59d72b8df24051ebb743081797e10467b0dadd
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-23 06:55:13 +0330

    is IsPalindrome problem in Leetcode

--- ProblemsOfLeetCodes/Problem1/Solution1.cs
+++ ProblemsOfLeetCodes/Problem1/Solution1.cs
@@ -50,6 +50,34 @@ namespace ProblemsOfLeetCodes.Problem1
             return uniquePointer;
         }
 
+        public static bool Palindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return true;
+
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left<right)
+            {
+                if (char.IsLetterOrDigit(s[left]))
+                    left++;
+                else if (char.IsLetterOrDigit(s[right]))
+                {
+                    right--;
+                }
+                else
+                {
+                    if (char.ToLower(s[left]) != char.ToLower(s[right])) return false;
+                }
+
+                left++;
+                right--;
+            }
+
+            return true;
+
+        }
+
 
     }
 }
