64e1f771b32775ac4a179593a272945ef8117bbe
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-30 10:59:00 +0330

    Two Sum Leetcode Problem In C#

---
+++ CSharpAndLeetCode/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CSharpAndLeetCode.TwoSumProblems;
+
+Console.WriteLine("Hello, World!");
+var nums = new int[]{20,7,11,15,7,2};
+Console.WriteLine($"{string.Join(",", Problems.TwoSum(nums, 9))}");
---
+++ CSharpAndLeetCode/TwoSumProblems/Problems.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAndLeetCode.TwoSumProblems
+{
+    public class Problems
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> seen = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int completed = target - nums[i];
+                if (seen.TryGetValue(completed, out int value))
+                {
+                    return new[] { i, value };
+                }
+                seen[nums[i]] = i;
+            }
+
+            return new[] { 0 };
+        }
+    }
+}
