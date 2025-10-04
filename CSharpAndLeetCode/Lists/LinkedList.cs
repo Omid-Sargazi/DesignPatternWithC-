39294679a461e2f8ee23bd3c4a8b0ab21dd60222
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-04 08:11:25 +0330

    Add and Print a LinkedList in C#

---
+++ CSharpAndLeetCode/Lists/LinkedList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAndLeetCode.Lists
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Next;
+
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+    public class LinkedList
+    {
+        private Node headNode;
+        public LinkedList()
+        {
+            
+        }
+
+        public LinkedList(Node node)
+        {
+            headNode = node;
+        }
+
+        public void Add(Node node)
+        {
+            if (headNode == null)
+            {
+                if (headNode == null)
+                {
+                    headNode = node;
+                    return;
+                }
+            }
+
+            var current = headNode;
+
+                while (current.Next !=null)
+                {
+                    current = current.Next;
+                }
+
+                current.Next = node;
+        }
+
+        public void Print()
+        {
+            if (headNode == null)
+            {
+                Console.WriteLine("List is Empty");
+                return;
+            }
+
+            var current = headNode;
+            while (current.Next != null)
+            {
+                Console.WriteLine(current.Data+"->");
+                current = current.Next;
+            }
+            Console.WriteLine("null");
+
+        }
+    }
+}
