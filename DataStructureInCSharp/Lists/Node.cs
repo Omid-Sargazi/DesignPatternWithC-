8261cc98267eb93c18524bdeff81d57e89895f4b
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-06 07:02:00 +0330

    LinkedList and Add item with O(1) complexity

---
+++ DataStructureInCSharp/Lists/Node.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureInCSharp.Lists
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Next { get; set; }
+        
+
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+           
+        }
+    }
+
+    public class LinkedList
+    {
+        private Node _head;
+        public int Count;
+        public LinkedList()
+        {
+            Count = 0;
+        }
+
+        public LinkedList(Node head)
+        {
+            _head = head;
+            Count = 0;
+        }
+
+
+        public void Add(int value)
+        {
+            if (_head == null)
+            {
+                _head = new Node(value);
+                Count++;
+                
+                return;
+            }
+
+            var current = _head;
+            while (current.Next !=null)
+            {
+                current = current.Next;
+            }
+
+            current.Next = new Node(value);
+            Count++;
+        }
+
+        public void Print()
+        {
+            if(_head==null) Console.WriteLine("List is Empty");
+            var current = _head;
+            while (current != null)
+            {
+                Console.Write(current.Data+"->");
+                current = current.Next;
+            }
+        }
+
+
+    }
+
+    public class ClientLinkedList
+    {
+        public static void Run()
+        {
+            LinkedList l1 = new LinkedList();
+            l1.Add(1);
+            l1.Add(11);
+            l1.Add(10);
+            l1.Add(21);
+            l1.Add(13);
+            l1.Print();
+        }
+    }
+}
--- DataStructureInCSharp/Program.cs
+++ DataStructureInCSharp/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using DataStructureInCSharp.Lists;
 using DataStructureInCSharp.Sorting;
 
 Console.WriteLine("Hello, World!");
 int[] nums = new int[]{7,4,10,10,11,12,-89,-7};
 KindOfSorting.Bubble(nums);
+
+ClientLinkedList.Run();
