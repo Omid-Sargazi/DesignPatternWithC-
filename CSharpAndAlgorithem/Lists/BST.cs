b1495f0e440cbc75781a977032b4e5533b9cf03a
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-06 09:03:44 +0330

    .

---
+++ CSharpAndAlgorithem/Lists/BST.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAndAlgorithem.Lists
+{
+    public class TreeNode
+    {
+        public int Value { get; set; }
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+
+        public TreeNode(int value)
+        {
+            Value = value;
+            Left = null;
+            Right = null;
+        }
+    }
+    public class BST
+    {
+        private TreeNode _root;
+
+        public BST()
+        {
+            _root = null;
+        }
+
+        public BST(int value)
+        {
+            _root = new TreeNode(value);
+        }
+
+        public void Insert(int value)
+        {
+            _root = InsertRec(_root, value);
+        }
+
+        public TreeNode InsertRec(TreeNode root, int value)
+        {
+            if (root == null)
+            {
+                root = new TreeNode(value);
+                return root;
+            }
+
+            if (value < root.Value)
+            {
+                root.Left = InsertRec(root.Left, value);
+                
+            }
+            else if(value>root.Value)
+            {
+                root.Right = InsertRec(root.Right, value);
+            }
+
+            return root;
+        }
+
+        
+
+
+
+    }
+}
