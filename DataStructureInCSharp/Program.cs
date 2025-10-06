afbc5b1ddb73ff7578441f030c239dbf7886f6aa
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-06 06:18:25 +0330

    Bubble Sorting in C# with O(n^2) time complexity

---
+++ DataStructureInCSharp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DataStructureInCSharp.Sorting;
+
+Console.WriteLine("Hello, World!");
+int[] nums = new int[]{7,4,10,10,11,12,-89,-7};
+KindOfSorting.Bubble(nums);
---
+++ DataStructureInCSharp/Sorting/KindOfSorting.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureInCSharp.Sorting
+{
+    public class KindOfSorting
+    {
+        public static void Bubble(int[] nums)
+        {
+            for (int start = nums.Length - 1; start >= 0; start--)
+            {
+                bool swapped = false;
+                for (int j = 0; j < start; j++)
+                {
+                    if (nums[j] > nums[j + 1])
+                    {
+                        (nums[j], nums[j+1]) = (nums[j+1],nums[j]);
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"{string.Join(",",nums)}");
+        }
+    }
+}
