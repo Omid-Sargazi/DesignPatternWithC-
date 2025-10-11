063feca4e40245cbbbaab4ee83155bdd5d43a08f
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-11 06:48:53 +0330

    Mediator is a behavioral design pattern that reduces coupling between components of a program by making them communicate indirectly, through a special mediator object.

---
+++ RefactoringGuru/BehavioralDesignPattern/IChatMediator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactoringGuru.BehavioralDesignPattern
+{
+    public class User
+    {
+        private IChatMediator _chatMediator;
+        public string Name { get; }
+
+        public User(string name, IChatMediator chatMediator)
+        {
+            Name = name;
+            _chatMediator = chatMediator;
+            _chatMediator.Register(this);
+        }
+
+        public void SendMessage(string message)
+        {
+            Console.WriteLine($"{Name} sends {message}");
+            _chatMediator.SendMessage(message,this);
+        }
+
+        public void ReceiveMessage(string message,User sender)
+        {
+            Console.WriteLine($"{Name} received from {sender.Name}:{message}");
+        }
+    }
+    public interface IChatMediator
+    {
+        void SendMessage(string message, User sender);
+        void Register(User user);
+    }
+
+    public class ChatMediator : IChatMediator
+    {
+        private List<User> _users = new List<User>();
+        public void SendMessage(string message, User sender)
+        {
+            foreach (var user in _users)
+            {
+                if (user != sender)
+                {
+                    user.ReceiveMessage(message,sender);
+                }
+            }
+        }
+
+        public void Register(User user)
+        {
+            if (!_users.Contains(user))
+            {
+                _users.Add(user);
+            }
+        }
+    }
+}
