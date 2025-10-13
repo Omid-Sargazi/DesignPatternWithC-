9f99429e25561fd3cd18c8efe915bbaea3b83d1a
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-13 09:55:17 +0330

    Longest Substring Without Repeating Characters

---
+++ CSharpAndAlgorithem/LeetCodeProblems/LongestSubstringWithoutRepeatingChar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAndAlgorithem.LeetCodeProblems
+{
+    public class LongestSubstringWithoutRepeatingChar
+    {
+        public static int Run(string s)
+        {
+            HashSet<char> seen = new HashSet<char>();
+            int left = 0;
+            int longest = 0;
+            int right = 0;
+            for (right = 0; right < s.Length; right++)
+            {
+                if (seen.Contains(s[right]))
+                {
+                    seen.Remove(s[left]);
+                    left++;
+                }
+
+                seen.Add(s[right]);
+                longest = Math.Max(longest, right - left+1);
+            }
+
+            
+
+            return longest;
+        }
+    }
+}
--- CSharpAndAlgorithem/Program.cs
+++ CSharpAndAlgorithem/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using CSharpAndAlgorithem.LeetCodeProblems;
 using CSharpAndAlgorithem.Lists;
 
 Console.WriteLine("Hello, World!");
 int[] arr = new[] { 1, 7, -9, 74, -10, 77, 41, 0, 0, 2 };
-CLientLists.Run();
+//CLientLists.Run();
+var res = LongestSubstringWithoutRepeatingChar.Run("Omidddd");
+Console.WriteLine(res);
