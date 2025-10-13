cb787076e0790bdfc93f378055d27401237045fc
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-13 06:43:04 +0330

    Mediator Pattern

---
+++ APIProjects/MediatorPattern/IChatRoom.cs
@@ -0,0 +1,60 @@
+﻿namespace APIProjects.MediatorPattern
+{
+    public class User
+    {
+        public string Name { get; set; }
+        private IChatRoom _chatRoom;
+
+        public User(string name, IChatRoom chatRoom)
+        {
+            _chatRoom = chatRoom;
+            Name = name;
+            _chatRoom.Register(this);
+        }
+
+        public void SendMessage(string message)
+        {
+            _chatRoom.SendMessage(message,this);
+        }
+
+        public void ReceiveMessage(string message, User sender)
+        {
+            Console.WriteLine($"Message:{message} receive by {Name} and sender is {sender} ");
+        }
+    }
+    public interface IChatRoom
+    {
+        void Register(User user);
+        void SendMessage(string msg, User sender);
+    }
+
+    public class ChatRoom:IChatRoom
+    {
+        private readonly List<User> _users= new List<User>();
+        public void Register(User user)
+        {
+            if (!_users.Contains(user))
+            {
+                _users.Add(user);
+            }
+        }
+
+        public void SendMessage(string msg, User sender)
+        {
+            foreach (var user in _users)
+            {
+                if (user != sender)
+                {
+                    user.ReceiveMessage(msg,sender);
+                }
+            }
+        }
+    }
+
+    public interface IRequest<TResult>{}
+
+    public interface IRequestHandler<TResult, TRequest>
+    {
+        TResult Handle(TRequest request);
+    }
+}
