b4c07fcc8f70b6d0a95330f84b0f2f94985b82f1
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-14 06:21:54 +0330

    Mediator is a behavioral design pattern that lets you reduce chaotic dependencies between objects. The pattern restricts direct communications between the objects and forces them to collaborate only via a mediator object.

---
+++ CSharpAndAlgorithem/BehavioralDesignPatterns/Mediator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAndAlgorithem.BehavioralDesignPatterns
+{
+    public class User
+    {
+        private readonly IMediator _mediator;
+        public string Name { get; set; }
+        public User(IMediator mediator, string name)
+        {
+            _mediator = mediator;
+            Name = name;
+            _mediator.Register(this);
+        }
+
+        public void SendMessage(string msg)
+        {
+            _mediator.SendMessage(msg,this);
+        }
+
+        public void ReceiveMessage(string msg, User sender)
+        {
+            Console.WriteLine($"This Message:{msg} Received from {sender} by {Name} ");
+        }
+    }
+    public interface IMediator
+    {
+        void Register(User user);
+        void SendMessage(string meg, User sender);
+    }
+    public class Mediator : IMediator
+    {
+        private readonly List<User> _users;
+
+        public Mediator()
+        {
+            _users = new List<User>();
+           
+        }
+        public string Name { get; set; }
+        public void Register(User user)
+        {
+            if (!_users.Contains(user))
+            {
+                _users.Add(user);
+            }
+        }
+
+        public void SendMessage(string msg, User sender)
+        {
+            foreach (var user in _users)
+            {
+                if (sender != user)
+                {
+                    user.ReceiveMessage(msg,sender);
+                }
+            }
+        }
+    }
+}
