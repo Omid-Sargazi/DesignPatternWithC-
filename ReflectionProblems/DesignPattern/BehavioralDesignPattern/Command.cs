005708943c938bbe9679e7e2d429f27c2c3dedf8
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-18 06:45:40 +0330

    Command in C#

---
+++ ReflectionProblems/DesignPattern/BehavioralDesignPattern/Command.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionProblems.DesignPattern.BehavioralDesignPattern
+{
+    public interface ICommand
+    {
+        void Execute();
+    }
+
+    public class SimpleCommand : ICommand
+    {
+        private string _payload = default;
+
+        public SimpleCommand(string palyload)
+        {
+            _payload = palyload;
+        }
+        public void Execute()
+        {
+            Console.WriteLine($"SimpleCommand: See, I can do simple things like printing ({this._payload})");
+        }
+    }
+
+    public class ComplexCommand : ICommand
+    {
+        private Receiver _receiver;
+
+        private string _a;
+        private string _b;
+
+        public ComplexCommand(Receiver receiver, string a, string b)
+        {
+            _a = a;
+            _b = b;
+            _receiver = receiver;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("ComplexCommand: Complex stuff should be done by a receiver object.");
+            _receiver.DoSomething(_a);
+            _receiver.DoSomethingElse(_b);
+        }
+    }
+
+    public class Receiver
+    {
+        public void DoSomething(string a)
+        {
+            Console.WriteLine($"Receiver: Working on ({a}.)");
+        }
+
+        public void DoSomethingElse(string b)
+        {
+            Console.WriteLine($"Receiver: Also working on ({b}.)");
+        }
+    }
+
+    public class Invoker
+    {
+        private ICommand _onStart;
+        private ICommand _onFinish;
+
+        public void SetOnStart(ICommand command)
+        {
+            _onStart = command;
+        }
+
+        public void SetOnFinish(ICommand command)
+        {
+            _onFinish = command;
+        }
+
+        public void DoSomethingImportant()
+        {
+            Console.WriteLine("Invoker: Does anybody want something done before I begin?");
+
+            if (_onStart is ICommand)
+            {
+                _onStart.Execute();
+            }
+            Console.WriteLine("Invoker: ...doing something really important...");
+
+            Console.WriteLine("Invoker: Does anybody want something done after I finish?");
+
+            if (_onFinish is ICommand)
+            {
+                _onFinish.Execute();
+            }
+
+        }
+
+
+    }
+}
