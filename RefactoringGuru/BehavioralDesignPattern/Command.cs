ab6547b01d01412febde848c5e1b3af2ab969fc7
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-18 06:25:11 +0330

    Command is behavioral design pattern that converts requests or simple operations into objects.

---
+++ RefactoringGuru/BehavioralDesignPattern/Command.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactoringGuru.BehavioralDesignPattern
+{
+    public interface ICommand
+    {
+        void Execute();
+    }
+
+    public class SimpleCommand : ICommand
+    {
+        private string _payload = string.Empty;
+
+        public  SimpleCommand(string payload)
+        {
+            _payload = payload;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine($"SimpleCommand: See, I can do simple things like printing ({this._payload})");
+        }
+    }
+
+    public class ComplexCommand:ICommand
+    {
+        private Receiver _receiver;
+        private string _a;
+        private string _b;
+
+        public ComplexCommand(Receiver receiver, string a, string b)
+        {
+            _a = a;
+            _b = b;
+            _receiver = receiver;
+        }
+        public void Execute()
+        {
+            Console.WriteLine("ComplexCommand: Complex stuff should be done by a receiver.");
+            _receiver.DoSomething(_a);
+            _receiver.DoSomethingElse(_b);
+        }
+    }
+
+    public class Receiver
+    {
+        public void DoSomething(string a)
+        {
+            Console.WriteLine($"Receiver: Working on ({a}.)");
+        }
+
+        public void DoSomethingElse(string b)
+        {
+            Console.WriteLine($"Receiver: Also working on ({b}.)");
+        }
+    }
+}
