86789d267441ba1cd6d0395a517ae28d7b18c52b
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-20 07:03:32 +0330

    get class for each interfaces

---
+++ ReflectionsInCSharp1/Problems/FindClasses.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionsInCSharp1.Problems
+{
+    public interface A
+    {
+        void MethodA();
+    }
+    public interface B
+    {}
+    public interface E
+    { }
+    public class C:A
+    {
+        public void MethodA()
+        {
+            Console.WriteLine("Here is Class C");
+        }
+    }
+    public class D :A,B
+    {
+        public void MethodA()
+        {
+            Console.WriteLine("Here is Class D");
+        }
+    }
+
+    public class F : A,B,E
+    {
+        public void MethodA()
+        {
+            Console.WriteLine("Here is Class F");
+        }
+    }
+
+    public class ClientReflection
+    {
+        public static void Run(object obj)
+        {
+           Type type =  obj.GetType();
+           Console.WriteLine(type.IsClass);
+
+           var interfaces = type.GetInterfaces();
+
+           foreach (var i in interfaces)
+           {
+               Console.WriteLine(i.Name);
+           }
+
+        }
+    }
+}
---
+++ ReflectionsInCSharp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using ReflectionsInCSharp1.Problems;
+
+namespace ReflectionsInCSharp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            C c = new C();
+            F f = new F();
+            ClientReflection.Run(f);
+        }
+    }
+}
