9b5486cf1db13d900dba67d88ac2bc4332525d0c
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-20 06:20:07 +0330

    Redfection InC#

---
+++ ReflectionIntefaces/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+
+namespace ReflectionIntefaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var interfaces = typeof(Test).GetInterfaces();
+
+            //interfaces = interfaces.Except(interfaces.SelectMany(x => x.GetInterfaces())).ToArray();
+
+            var totalTypes = Assembly.GetExecutingAssembly().GetTypes();
+
+            var i = 0;
+
+            var HasCInterface = totalTypes.Where(x => x.GetInterfaces().Any(a => a.Name == typeof(A).Name)).ToList();
+
+            i++;
+
+        }
+    }
+
+    public interface A : B
+    {
+
+    }
+    public interface B
+    {
+
+    }
+    public interface C
+    {
+
+    }
+
+    public abstract class ABS : A
+    {
+
+    }
+
+    public class Test : ABS
+    {
+
+    }
+
+    public class Person : Test, C
+    {
+
+    }
+}
