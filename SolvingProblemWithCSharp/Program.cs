bdd6c187f5470806126b2e534881fc38ee4b5265
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-21 06:31:31 +0330

    Bubble Sorting in C#

---
+++ SolvingProblemWithCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using SolvingProblemWithCSharp.Sorting;
+
+namespace SolvingProblemWithCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[] arr = new int[] { 1, -2, 30,28,0,-1,-7,7,8,10 };
+            CommonSorting.BubbleSort(arr);
+        }
+    }
+}
---
+++ SolvingProblemWithCSharp/Sorting/CommonSorting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolvingProblemWithCSharp.Sorting
+{
+    public class CommonSorting
+    {
+        public static void BubbleSort(int[] arr)
+        {
+            Console.WriteLine($"Before Sorting:  {string.Join(",",arr)}");
+            int n = arr.Length;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                bool swapped = false;
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                        swapped = true;
+                    }
+                }
+                if(swapped==false) break;
+            }
+            Console.WriteLine($"After Sorting:  {string.Join(",",arr)}");
+        }
+    }
+}
