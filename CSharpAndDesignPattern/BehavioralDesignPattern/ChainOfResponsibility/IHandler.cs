d2ca63721918a2971887c73e83ffa3e64e9a5839
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-25 06:28:54 +0330

    Complete Pattern

--- CSharpAndDesignPattern/BehavioralDesignPattern/ChainOfResponsibility/IHandler.cs
+++ CSharpAndDesignPattern/BehavioralDesignPattern/ChainOfResponsibility/IHandler.cs
@@ -50,4 +50,34 @@ namespace CSharpAndDesignPattern.BehavioralDesignPattern.ChainOfResponsibility
             }
         }
     }
+
+    class SquirrelHandler : AbstractHandler
+    {
+        public override object Handle(object request)
+        {
+            if (request.ToString() == "Nut")
+            {
+                return $"Squirrel: I'll eat the {request.ToString()}.\n";
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+    }
+
+    class DogHandler : AbstractHandler
+    {
+        public override object Handle(object request)
+        {
+            if (request.ToString() == "MeatBall")
+            {
+                return $"Dog: I'll eat the {request.ToString()}.\n";
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+    }
 }
