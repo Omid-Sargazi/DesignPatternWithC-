f50cb624c60618193e15916c80db036073e866a3
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-25 09:38:47 +0330

    hain of Responsibility is a behavioral design pattern that lets you pass requests along a chain of handlers. Upon receiving a request, each handler decides either to process the request or to pass it to the next handler in the chain.

---
+++ CSharpAndDesignPattern/BehavioralDesignPattern/ChainOfResponsibility/RequestHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAndDesignPattern.BehavioralDesignPattern.ChainOfResponsibility
+{
+    public enum ReqType
+    {
+        SimpleReq = 1,
+        TechnicalReq = 2,
+        ComplexReq = 3,
+        FinancialReq = 4
+    };
+
+    public interface IRequestHandler
+    {
+        IRequestHandler Handle(ReqType request,IRequestHandler  req);
+    }
+
+    public abstract class RequestHandler : IRequestHandler
+    {
+        private IRequestHandler _nextHandler;
+        public IRequestHandler Handle(ReqType request, IRequestHandler req)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+   
+}
---
+++ CSharpAndDesignPattern/BehavioralDesignPattern/ChainOfResponsibility/UserRequest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAndDesignPattern.BehavioralDesignPattern.ChainOfResponsibility
+{
+    public class UserRequest
+    {
+        public int UserId { get; set; }
+        public string Role { get; set; }
+        public bool Permissions { get; set; }
+    }
+
+    public interface IHandlerr
+    {
+        IHandlerr SetNext(IHandlerr next);
+        void Handle(UserRequest request);
+    }
+
+    public abstract class HandlerBase : IHandlerr
+    {
+        private IHandlerr _next;
+        public IHandlerr SetNext(IHandlerr next)
+        {
+            _next = next;
+            return this;
+        }
+
+        protected virtual void PassToNext(UserRequest request)
+        {
+            _next?.Handle(request);
+        }
+
+        public abstract void Handle(UserRequest request);
+    }
+
+    public class AuthenticationHandler : HandlerBase
+    {
+        public override void Handle(UserRequest request)
+        {
+            if (request.Role == "Auth")
+            {
+                Console.WriteLine("Authentication");
+            }
+            else
+            {
+                PassToNext(request);
+            }
+        }
+    }
+
+    public class AuthorizationHandler:HandlerBase
+    {
+        public override void Handle(UserRequest request)
+        {
+            if (request.Role == "Authorization")
+            {
+                Console.WriteLine("Authorization");
+            }
+            else
+            {
+                PassToNext(request);
+            }
+        }
+    }
+
+    public class ValidationHandler : HandlerBase
+    {
+        public override void Handle(UserRequest request)
+        {
+            if (request.Role == "Valid")
+            {
+                Console.WriteLine("Validation");
+            }
+            else
+            {
+                PassToNext(request);
+            }
+        }
+    }
+}
