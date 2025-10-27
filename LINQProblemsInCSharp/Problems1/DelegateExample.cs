22c161fc026ecdfa82bfa44afa803b9793e7c3ad
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-27 06:42:35 +0330

    Simple Delegate in C#

---
+++ LINQProblemsInCSharp/Problems1/DelegateExample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProblemsInCSharp.Problems1
+{
+
+    public delegate int Calculator(int a, int b);
+    public class DelegateExample
+    {
+        public void Run(int a, int b)
+        {
+            Calculator calculator = Add;
+            Console.WriteLine(calculator(a,b));
+            calculator += Mul;
+            Console.WriteLine(calculator(a,b));
+        }
+        public int Add(int a, int b) => a + b;
+        public int Mul(int a, int b) => a * b;
+    }
+}
---
+++ LINQProblemsInCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using LINQProblemsInCSharp.Problems1;
+
+namespace LINQProblemsInCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            DelegateExample example = new DelegateExample();
+            example.Run(3,2);
+        }
+    }
+}
