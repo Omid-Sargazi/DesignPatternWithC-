d176f0587eff46a82c9b2b98930e63ad95622149
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-27 06:35:29 +0330

    Chain of Responsibility in C#

---
+++ CSharpAndLeetCode/LINQProject/Problem1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAndLeetCode.LINQProject
+{
+    public interface IHandler
+    {
+        IHandler SetNext(IHandler handler);
+        object Handle(object request);
+    }
+
+    public class AbstractHandler:IHandler
+    {
+        private IHandler _handler;
+        public IHandler SetNext(IHandler handler)
+        {
+            _handler = handler;
+
+            return handler;
+        }
+
+        public virtual object Handle(object request)
+        {
+            if (_handler != null)
+            {
+               return _handler.Handle(request);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public class MonkeyHandler : AbstractHandler
+        {
+            public override object Handle(object request)
+            {
+                if ((request as string) == "Banaan")
+                {
+                    return $"Monkey: I'll eat the {request.ToString()}.\n";
+                }
+                else
+                {
+                    return base.Handle(request);
+                }
+            }
+        }
+    }
+}
