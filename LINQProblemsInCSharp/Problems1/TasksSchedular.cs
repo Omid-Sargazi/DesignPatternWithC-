e24f1383090121865105c76082205fc8e832bbfd
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-27 09:54:54 +0330

    Execute Task in specific time

---
+++ LINQProblemsInCSharp/Problems1/TasksSchedular.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProblemsInCSharp.Problems1
+{
+    public interface ITasks
+    {
+        void Execute();
+    }
+    public class Tasks:ITasks
+    {
+        public void Execute()
+        {
+            Console.WriteLine($"Execute a Task.{DateTime.Now}");
+        }
+    }
+
+    public class TaskSchedular
+    {
+        //private List<ITasks> _tasks = new List<ITasks>();
+        private readonly ITasks _tasks;
+
+        public TaskSchedular(ITasks tasks)
+        {
+            //_tasks.Add(tasks);
+            _tasks = tasks;
+        }
+
+        public void ExecuteAt10()
+        {
+            while (true)
+            {
+                int h = DateTime.Now.Hour;
+                int m = DateTime.Now.Minute;
+                int s = DateTime.Now.Second;
+
+                if (h == 10 && m == 0 && s == 0)
+                {
+                    _tasks.Execute();
+                    break;
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
