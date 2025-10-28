7901174502fecf5c4167da2f127a396c4a75f025
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-28 06:25:16 +0330

    create classes and interfaces for gettting in assembly

---
+++ ReflectionsInCSharp/Problem01/Interfaces.cs
@@ -0,0 +1,53 @@
+﻿using System.Reflection;
+
+namespace ReflectionsInCSharp.Problem01
+{
+    public interface A
+    {
+        void MethodA();
+    }
+
+    public interface B
+    {
+        void MethodB();
+    }
+
+    public interface C
+    {
+        void MethodC();
+    }
+
+    public class AB:A,B
+    {
+        public void MethodA()
+        {
+            Console.WriteLine("There is AB Class and MethodA ");
+        }
+
+        public void MethodB()
+        {
+            Console.WriteLine("There is AB Class and MethodB");
+        }
+    }
+
+    public class BC:B,C
+    {
+        public void MethodB()
+        {
+            Console.WriteLine("There is BC Class and MethodB ");
+        }
+
+        public void MethodC()
+        {
+            Console.WriteLine("There is BC Class and MethodC ");
+        }
+    }
+
+    public class AssemblyInterfaceMapper
+    {
+        public void Execute(Assembly assembly)
+        {
+
+        }
+    }
+}
