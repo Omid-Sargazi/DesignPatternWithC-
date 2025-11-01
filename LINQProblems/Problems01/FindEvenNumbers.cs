0fd7404d29405161186b2abc384255ea43b65e83
Omid-Sargazi <om.sargazi@gmail.com>
2025-11-01 06:45:08 +0330

    Where, OrderbyDesending, OrderBy and GroupBy

--- LINQProblems/Problems01/FindEvenNumbers.cs
+++ LINQProblems/Problems01/FindEvenNumbers.cs
@@ -20,6 +20,45 @@ namespace LINQProblems.Problems01
 
             var res3 = nums1.Sum();
 
+            var people = new List<Person>
+            {
+                new Person { Name = "Omid", Age = 43 },
+                new Person { Name = "Saeed", Age = 39 },
+                new Person { Name = "Saleh", Age = 15 },
+                new Person { Name = "Karim", Age = 55 },
+            };
+
+            var res4 = people.Max(p => p.Age);
+
+
+            var res5 = people.GroupBy(p => p.Name[0]);
+            foreach (var group in res5)
+            {
+                Console.WriteLine($"Group:{group.Key}");
+                foreach (var person in group)
+                {
+                    Console.WriteLine($"--{person.Name}");
+                }
+            }
+
+
+            var res6 = people.OrderByDescending(p => p.Age).ToList();
+            var res7 = people.FirstOrDefault(p=>p.Age>25);
+            if (res7 != null)
+            {
+                Console.WriteLine($"{res7.Name},{res7.Age}");
+            }
+
+            var res8 = 
+
         }
     }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+
 }
