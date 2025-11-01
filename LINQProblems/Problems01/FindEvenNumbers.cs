89cb6c483546670725c49c1fbc1fde26a091f743
Omid-Sargazi <om.sargazi@gmail.com>
2025-11-01 06:27:42 +0330

    Find Even numbers in a list with Where

---
+++ LINQProblems/Problems01/FindEvenNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProblems.Problems01
+{
+    public class FindEvenNumbers
+    {
+        public static void Run()
+        {
+            var nums1 = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var res1 = nums1.Where(n => n % 2 == 0).ToList();
+
+        }
+    }
+}
