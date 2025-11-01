e8fd22de4791c64fcc8d2bb29ec57c4545de2015
Omid-Sargazi <om.sargazi@gmail.com>
2025-11-01 08:58:49 +0330

    implement multicast delegate
    - Add multiple methods to delegate using +=
    - Remove methods using -=
    - Use GetInvocationList to see registered methods
    - Understand execution order in multicast"

--- LINQProblems/Problems01/Observer.cs
+++ LINQProblems/Problems01/Observer.cs
@@ -24,6 +24,8 @@ namespace LINQProblems.Problems01
 
     public delegate void SimpleDelegate(string message);
 
+    public delegate void MultiDelegate(string message);
+
     public class Problem2
     {
         public  void DisplayMessage(string txt)
@@ -35,6 +37,31 @@ namespace LINQProblems.Problems01
         public void ShowWarning(string warning)
         {
             Console.WriteLine($"Warning: {warning}");
+        }
+
+        public static void MethodA(string msg)
+        {
+            Console.WriteLine($"MethodA: {msg}");
+        }
+
+        static void MethodB(string msg)
+        {
+            Console.WriteLine($"MethodB: {msg.ToUpper()}");
+        }
+
+        static void MethodC(string msg)
+        {
+            Console.WriteLine($"MethodC: {msg} - طول: {msg.Length}");
+        }
+
+        static void MethodD(string msg)
+        {
+            Console.WriteLine($"MethodD: {msg.ToUpper()}");
+        }
+
+        static void MethodE(string msg)
+        {
+            Console.WriteLine($"MethodE: {msg} - طول: {msg.Length}");
         }
 
 
@@ -51,6 +78,29 @@ namespace LINQProblems.Problems01
             myDelegate("This is a warning");
 
             Console.WriteLine("This completed");
+        }
+
+        public static void Run2()
+        {
+            Console.WriteLine("=== Multicast Delegate ===");
+
+            MultiDelegate multiDelegate = MethodA;
+            multiDelegate += MethodB;
+            multiDelegate += MethodC;
+
+            Console.WriteLine("call all methods");
+
+            multiDelegate("Hello Multicast");
+
+            Console.WriteLine("Delete a method");
+            multiDelegate -= MethodB;
+
+            Console.WriteLine("after delete a method");
+
+            if (multiDelegate != null)
+            {
+                Console.WriteLine($"{multiDelegate.GetInvocationList().Length}");
+            }
         }
 
 
