20f58faa7d4cd49ac7531a24a2cb54f7cff57573
Omid-Sargazi <om.sargazi@gmail.com>
2025-11-22 06:47:26 +0330

    the Iterator, clients can go over elements of different collections in a similar fashion using a single iterator interface.

---
+++ DesignPattern/BehavioralDesignPatternSecond/SimpleCollection.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.BehavioralDesignPatternSecond
+{
+    public class SimpleCollection2
+    {
+        private string[] _items = { "Apple", "Banana", "Cherry" };
+
+        public string GetItem(int index)
+        {
+            return _items[index];
+        }
+
+        public int Count => _items.Length;
+    }
+
+    public interface IIterator
+    {
+        bool HasNext();
+        object Next();
+    }
+
+    public class SimpleIterator2:IIterator
+    {
+        private SimpleCollection2 _collection;
+        private int _currentIndex = 0;
+
+        public SimpleIterator2(SimpleCollection2 collection)
+        {
+            _collection = collection;
+        }
+        public bool HasNext()
+        {
+            return _currentIndex < _collection.Count;
+        }
+
+        public object Next()
+        {
+            if (HasNext())
+            {
+                throw new InvalidOperationException("No more items");
+            }
+
+            var item = _collection.GetItem(_currentIndex);
+            _currentIndex++;
+            return item;
+        }
+    }
+}
