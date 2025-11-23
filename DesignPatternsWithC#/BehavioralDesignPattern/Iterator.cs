c52a25957dcc0ef7f5ad320bda6e6d7d7158fd0b
Omid-Sargazi <om.sargazi@gmail.com>
2025-11-23 06:29:45 +0330

    Iterator is a behavioral design pattern that allows sequential traversal through a complex data structure without exposing its internal details.

---
+++ DesignPatternsWithC#/BehavioralDesignPattern/Iterator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsWithC_.BehavioralDesignPattern
+{
+    public class SimpleCollection
+    {
+        private string[] _items = { "Apple", "Banana", "Cherry" };
+
+        public string GetItem(int index)
+        {
+            return _items[index];
+        }
+
+        public int Count=> _items.Length;
+    }
+
+    public interface IIterator
+    {
+        bool HasNext();
+        object Next();
+    }
+
+    public class SimpleIterator:IIterator
+    {
+        private SimpleCollection _collection;
+        private int _currentIndex = 0;
+
+        public SimpleIterator(SimpleCollection collection)
+        {
+            _collection = collection;
+        }
+        public bool HasNext()
+        {
+            return _currentIndex < _collection.Count;
+        }
+
+        public object Next()
+        {
+            if (!HasNext())
+            {
+                throw new InvalidOperationException("No more items");
+            }
+
+            var item = _collection.GetItem(_currentIndex);
+            _currentIndex++;
+            return item;
+        }
+    }
+}
