7c3638db74be2487e2ed9e4d9aaf9233eb379f90
Omid-Sargazi <om.sargazi@gmail.com>
2025-12-03 06:14:38 +0330

    .

---
+++ AuthProblem1/Program.cs
@@ -0,0 +1,39 @@
+
+namespace AuthProblem1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.UseStaticFiles();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
---
+++ DesignPatternsInCSharp/StructuralDesignPattern/IContentComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.StructuralDesignPattern
+{
+    public interface IContentComponent
+    {
+        void Add(IContentComponent  component);
+        void Remove(IContentComponent component);
+        int GetTotalWords();
+        double GetReadingTime(int wordPerMinu = 200);
+        void DispalyHirachy(int indent=0);
+        List<IContentComponent> Search(string keyWord);
+    }
+
+    
+}
+
+//2680210255
+//P @risa1373
