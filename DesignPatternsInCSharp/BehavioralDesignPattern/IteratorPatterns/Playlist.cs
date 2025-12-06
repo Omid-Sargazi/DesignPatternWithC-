6a3a9a49fb00770ffdb74311bad35c74c5e08e94
Omid-Sargazi <om.sargazi@gmail.com>
2025-12-06 06:10:20 +0330

    PlayList Without PlayList

---
+++ DesignPatternsInCSharp/BehavioralDesignPattern/IteratorPatterns/Playlist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.BehavioralDesignPattern.IteratorPatterns
+{
+    public class Playlist
+    {
+        private string[] _songs = { "Song1", "Song2", "Song3", "Song4" };
+
+        public string GetSong(int index) => _songs[index];
+        public int SongCount => _songs.Length;
+    }
+}
