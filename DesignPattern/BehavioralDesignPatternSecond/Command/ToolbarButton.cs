f93185c92c1b62e1539e6244828447feecd0c224
Omid-Sargazi <om.sargazi@gmail.com>
2025-12-08 06:38:17 +0330

    Complete Command Pattern

--- DesignPattern/BehavioralDesignPatternSecond/Command/ToolbarButton.cs
+++ DesignPattern/BehavioralDesignPatternSecond/Command/ToolbarButton.cs
@@ -42,4 +42,99 @@ namespace DesignPattern.BehavioralDesignPatternSecond.Command
 
         public string GetContent() => _content;
     }
+
+
+    public class WriteCommand : ICommand2
+    {
+        private Document _document;
+        private string _text;
+
+        public WriteCommand(Document document, string text)
+        {
+            _document = document;
+            _text = text;
+        }
+
+        public void Execute()
+        {
+            _document.Write(_text);
+        }
+
+        public void Undo()
+        {
+            // In a real scenario, we'd use proper memento or history tracking
+            _document.Erase(_text.Length);
+        }
+    }
+
+    public class EraseCommand : ICommand2
+    {
+        private Document _document;
+        private int _length;
+        private string _erasedText;
+
+        public EraseCommand(Document document, int length)
+        {
+            _document = document;
+            _length = length;
+            // Store what we're about to erase for undo
+            string current = document.GetContent();
+            _erasedText = current.Length > length
+                ? current.Substring(current.Length - length)
+                : current;
+        }
+
+        public void Execute()
+        {
+            _document.Erase(_length);
+        }
+
+        public void Undo()
+        {
+            _document.Write(_erasedText);
+        }
+    }
+
+    // 4. Invoker (UI Components, Scheduler, etc.)
+    public class Button
+    {
+        private ICommand2 _command;
+        private string _label;
+
+        public Button(string label, ICommand2 command)
+        {
+            _label = label;
+            _command = command;
+        }
+
+        public void Click()
+        {
+            Console.WriteLine($"\nButton '{_label}' clicked:");
+            _command.Execute();
+        }
+
+        public void SetCommand(ICommand2 command)
+        {
+            _command = command;
+        }
+    }
+
+    public class KeyboardShortcut
+    {
+        private Dictionary<ConsoleKey, ICommand2> _shortcuts = new Dictionary<ConsoleKey, ICommand2>();
+
+        public void RegisterShortcut(ConsoleKey key, ICommand2 command)
+        {
+            _shortcuts[key] = command;
+        }
+
+        public void HandleKeyPress(ConsoleKey key)
+        {
+            if (_shortcuts.ContainsKey(key))
+            {
+                Console.WriteLine($"\nShortcut '{key}' pressed:");
+                _shortcuts[key].Execute();
+            }
+        }
+    }
 }
