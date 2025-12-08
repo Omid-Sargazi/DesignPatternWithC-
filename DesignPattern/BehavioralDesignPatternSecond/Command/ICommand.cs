8c2ca1af3c908be2355e31ef9efd993d182c1bc7
Omid-Sargazi <om.sargazi@gmail.com>
2025-12-08 06:26:11 +0330

    .

--- DesignPattern/BehavioralDesignPatternSecond/Command/ICommand.cs
+++ DesignPattern/BehavioralDesignPatternSecond/Command/ICommand.cs
@@ -11,4 +11,28 @@ namespace DesignPattern.BehavioralDesignPatternSecond.Command
         string Name { get; }
         void Execute();
     }
+
+    public class StartCommand:ICommand
+    {
+        public string Name
+        {
+            get { return "Start"; }
+        }
+        public void Execute()
+        {
+            Console.WriteLine("I am executing StartCommand");
+        }
+    }
+
+    public class StopCommand:ICommand
+    {
+        public string Name
+        {
+            get { return "Stop"; }
+        }
+        public void Execute()
+        {
+            Console.WriteLine("I am executing StopCommand");
+        }
+    }
 }
