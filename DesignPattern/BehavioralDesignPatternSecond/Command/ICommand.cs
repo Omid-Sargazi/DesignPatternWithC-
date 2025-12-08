87745cbd77bb999f9702b23a09ae580660249931
Omid-Sargazi <om.sargazi@gmail.com>
2025-12-08 06:23:45 +0330

    Interface for Command Pattern

---
+++ DesignPattern/BehavioralDesignPatternSecond/Command/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.BehavioralDesignPatternSecond.Command
+{
+    internal interface ICommand
+    {
+        string Name { get; }
+        void Execute();
+    }
+}
