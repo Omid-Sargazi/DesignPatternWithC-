b4c6d577b5a528bc56f61369b99c9118b982e792
Omid-Sargazi <om.sargazi@gmail.com>
2025-12-13 06:16:17 +0330

    Define and Create Entities

---
+++ LINQProblemsInCSharp/Problems1/Player.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProblemsInCSharp.Problems1
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TeamId { get; set; }
+        public string Position { get; set; } // "Forward", "Midfielder", "Defender", "Goalkeeper"
+        public int JerseyNumber { get; set; }
+        public DateTime JoinDate { get; set; }
+        public decimal Salary { get; set; }
+    }
+
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Coach { get; set; }
+        public string City { get; set; }
+        public DateTime FoundedYear { get; set; }
+    }
+
+    public class Match
+    {
+        public int Id { get; set; }
+        public int HomeTeamId { get; set; }
+        public int AwayTeamId { get; set; }
+        public DateTime MatchDate { get; set; }
+        public string Stadium { get; set; }
+        public int? HomeTeamScore { get; set; }
+        public int? AwayTeamScore { get; set; }
+        public string Status { get; set; } // "Scheduled", "Completed", "Cancelled"
+    }
+
+    public class PlayerStatistic
+    {
+        public int Id { get; set; }
+        public int PlayerId { get; set; }
+        public int MatchId { get; set; }
+        public int Goals { get; set; }
+        public int Assists { get; set; }
+        public int YellowCards { get; set; }
+        public int RedCards { get; set; }
+        public int MinutesPlayed { get; set; }
+    }
+}
