c04f000d4c0675cc3a2725a1d2839c1911aeb531
Omid-Sargazi <om.sargazi@gmail.com>
2025-12-20 06:20:12 +0330

    Add personal expense management system

---
+++ LINQProblems/IncomeManagment/Expense.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProblems.IncomeManagment
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; } // "Food", "Transport", "Entertainment", "Bills"
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string PaymentMethod { get; set; } // "Cash", "Card", "Online"
+        public bool IsRecurring { get; set; }
+    }
+
+    public class Category
+    {
+        public string Name { get; set; }
+        public decimal MonthlyBudget { get; set; }
+        public string Color { get; set; } // For UI purposes
+    }
+
+    public class Income
+    {
+        public int Id { get; set; }
+        public string Source { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsRegular { get; set; }
+    }
+}
