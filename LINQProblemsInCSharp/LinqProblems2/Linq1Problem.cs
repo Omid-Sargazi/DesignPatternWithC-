1a1c0a3b6c28a6f59484d0ac5e9dabcbc49a4150
Omid-Sargazi <om.sargazi@gmail.com>
2025-12-25 11:00:06 +0330

    .

--- LINQProblemsInCSharp/LinqProblems2/Linq1Problem.cs
+++ LINQProblemsInCSharp/LinqProblems2/Linq1Problem.cs
@@ -28,6 +28,21 @@ namespace LINQProblemsInCSharp.LinqProblems2
             var result2 = students.
                 Where(s => s.StartsWith("A", StringComparison.OrdinalIgnoreCase)).OrderBy(s => s);
             Console.WriteLine(string.Join(", ",result2));
+
+            var products = new[]
+            {
+                new {Name="laptop",Price=2500},
+                new {Name="Mouse",Price=50},
+                new {Name="Keyboard",Price=200},
+                new {Name="Monitor",Price=1200},
+                new {Name="Headphones",Price=300},
+            };
+
+            var result3 = products.Where(p => p.Price > 1000)
+                .OrderByDescending((p => p))
+                .Select(p => $"{p.Name},{p.Price}");
+            Console.WriteLine(string.Join(", ", result3));
+
         }
     }
 }
