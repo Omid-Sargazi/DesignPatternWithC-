571297f559255004b33f7b2c0065e1b2a4a5cc5b
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-19 09:59:18 +0330

    Abstraction and Implementation

--- DesignPatternsInCSharp/Bridge/IRenderer/IRenderer.cs
+++ DesignPatternsInCSharp/Bridge/IRenderer/IRenderer.cs
@@ -23,4 +23,25 @@ namespace DesignPatternsInCSharp.Bridge.IRenderer
         public void RenderCircle(float r) => Console.WriteLine($"DirectX: Drawing circle r={r}");
         public void RenderSquare(float s) => Console.WriteLine($"DirectX: Drawing square s={s}");
     }
+
+    public abstract class Shape
+    {
+        protected IRenderer _renderer;
+        public Shape(IRenderer renderer) => _renderer = renderer;
+        public abstract void Draw();
+    }
+
+    public class Circle : Shape
+    {
+        private float _radius;
+        public Circle(IRenderer renderer, float radius) : base(renderer) => _radius = radius;
+        public override void Draw() => _renderer.RenderCircle(_radius);
+    }
+
+    public class Square : Shape
+    {
+        private float _side;
+        public Square(IRenderer renderer, float side) : base(renderer) => _side = side;
+        public override void Draw() => _renderer.RenderSquare(_side);
+    }
 }
