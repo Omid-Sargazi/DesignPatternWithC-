04a9c32728d915b319a759b5353668298ce6997f
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-19 10:01:07 +0330

    Implementation

---
+++ DesignPatternsInCSharp/Bridge/MellatGateway.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.Bridge
+{
+    public interface IPaymentGateway2
+    {
+        bool Pay(decimal amount, string cardInfo);
+    }
+
+    public class ZarinpalGateway : IPaymentGateway2
+    {
+        public bool Pay(decimal amount, string cardInfo)
+        {
+            Console.WriteLine($"Zarinpal: پرداخت {amount} تومان");
+            return true;
+        }
+    }
+
+    public class MellatGateway : IPaymentGateway2
+    {
+        public bool Pay(decimal amount, string cardInfo)
+        {
+            Console.WriteLine($"Mellat: پرداخت {amount} تومان");
+            return true;
+        }
+    }
+}
