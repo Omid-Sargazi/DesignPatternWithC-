0cc093179e85ea628f5f8a21a89f3d9afeefe1c8
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-26 10:27:54 +0330

    Introduced Bridge pattern in game rendering abstraction

---
+++ DesignPatternsInCSharp/CompositionStrategyDecorator/OrderHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesignPatternsInCSharp.Bridge;
+
+namespace DesignPatternsInCSharp.CompositionStrategyDecorator
+{
+    public abstract class OrderHandler
+    {
+        protected OrderHandler Next;
+
+        public OrderHandler SetNext(OrderHandler next)
+        {
+            Next = next;
+            return next;
+        }
+
+        public virtual void Handle(Order order)
+        {
+            Next?.Handle(order);
+        }
+    }
+
+    public class ValidateHandler : OrderHandler
+    {
+        public override void Handle(Order order)
+        {
+            Console.WriteLine("Validating...");
+            base.Handle(order);
+        }
+    }
+
+}
