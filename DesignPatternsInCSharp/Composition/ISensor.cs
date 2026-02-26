4b853f85be69cb204e82a4901bbaa466ed46314a
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-26 09:57:23 +0330

    ...

---
+++ DesignPatternsInCSharp/Composition/ISensor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.Composition
+{
+    public interface ISensor
+    {
+        string Read();
+    }
+
+    public class TemperatureSensor : ISensor
+    {
+        public string Read() => "Temp: 22°C";
+    }
+
+    public class HumiditySensor : ISensor
+    {
+        public string Read() => "Humidity: 40%";
+    }
+
+    public class Device
+    {
+        private readonly List<ISensor> _sensors = new();
+
+        public void AddSensor(ISensor sensor) => _sensors.Add(sensor);
+
+        public void ReadAll()
+        {
+            foreach (var sensor in _sensors)
+                Console.WriteLine(sensor.Read());
+        }
+    }
+
+}
