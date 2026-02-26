4d2c91338019d43163b89bb57f3296a406a1dd8e
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-26 09:49:03 +0330

    Payment Way with Composition Pattern

---
+++ DesignPatternsInCSharp/Composition/IPaymentProcessor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.Composition
+{
+    public interface IPaymentProcessor { void Process(decimal amount); }
+    public class BasicPaymentProcessor : IPaymentProcessor { public void Process(decimal amount) { Console.WriteLine($"Processing payment: {amount}"); } }
+    public class LoggingDecorator : IPaymentProcessor { private readonly IPaymentProcessor _processor; public LoggingDecorator(IPaymentProcessor processor) { _processor = processor; } public void Process(decimal amount) { Console.WriteLine("Logging payment..."); _processor.Process(amount); } }
+}
