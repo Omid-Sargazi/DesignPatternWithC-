19ef5c2919c2137cd3f0441792ea99611beefa9b
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-26 09:55:56 +0330

    ..

---
+++ DesignPatternsInCSharp/Composition/GameObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.Composition
+{
+    public class GameObject
+    {
+        public IRenderer Renderer { get; set; }
+        public IPhysics Physics { get; set; }
+
+        public void Update()
+        {
+            Physics?.Update();
+            Renderer?.Render();
+        }
+    }
+
+    public interface IRenderer { void Render(); }
+    public interface IPhysics { void Update(); }
+
+}
