3271b1338f075c3fb1c249129df2d973ed4d8014
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-26 10:21:59 +0330

    Added Decorator-enhanced payment logging layer

---
+++ DesignPatternsInCSharp/CompositionStrategyDecorator/IRenderEngine.cs
@@ -0,0 +1,37 @@
+﻿using DesignPatternsInCSharp.Composition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.CompositionStrategyDecorator
+{
+    public interface IRenderEngine
+    {
+        void Draw();
+    }
+
+    public class DirectXRenderer : IRenderEngine
+    {
+        public void Draw() => Console.WriteLine("DX Render");
+    }
+
+    public class OpenGLRenderer : IRenderEngine
+    {
+        public void Draw() => Console.WriteLine("OpenGL Render");
+    }
+
+    public class GameObject
+    {
+        public IRenderEngine Renderer { get; set; }
+        public IPhysics Physics { get; set; }
+
+        public void Update()
+        {
+            Physics?.Update();
+            Renderer?.Draw();
+        }
+    }
+
+}
